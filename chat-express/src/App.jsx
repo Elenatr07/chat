c265dfbb6dfc22b8bd19197ab15a02679771f664
Elenatr07 <e.turkina2010@yandex.ru>
2022-12-25 14:41:02 +1000

    creat react

---
+++ chat-express/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+
+        </BrowserRouter>
+
+
+    );
+};
+
+
+
+export default App;
